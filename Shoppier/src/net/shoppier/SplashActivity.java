6b66c16e2e80ed319fe329b00320c2fe5392f297
Alex <ajschuit1@gmail.com>
2013-09-26 18:48:18 -0400

    I added what I have so far for the app.

---
+++ Shoppier/src/net/shoppier/SplashActivity.java
@@ -0,0 +1,37 @@
+package net.shoppier;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class SplashActivity extends Activity {
+	Button login;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		login = (Button) findViewById(R.id.button1);
+		login.setOnClickListener(handler);
+	}
+	private OnClickListener handler = new OnClickListener() {
+				
+		public void onClick(View v) {
+			if (v == login){
+				startActivity(new Intent(SplashActivity.this, MainActivity.class));
+			}
+		}
+	};
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.splash, menu);
+		return true;
+	}
+
+}
